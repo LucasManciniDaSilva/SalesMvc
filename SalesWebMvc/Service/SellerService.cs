c9ecc484fce0d8e19b8a8f7bbb1eff69c7f149cb
Lucas M da Silva <lucasmancini1997@gmail.com>
2019-03-14 10:47:37 -0300

    Delete Sellers

--- SalesWebMvc/Service/SellerService.cs
+++ SalesWebMvc/Service/SellerService.cs
@@ -25,6 +25,20 @@ namespace SalesWebMvc.Service
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+
+        public Sellers FindById(int id)
+        {
+            return _context.Sellers.FirstOrDefault(obj => obj.Id == id);
+        }
+
+
+        public void Remove(int id)
+        {
+            var obj = _context.Sellers.Find(id);
+            _context.Sellers.Remove(obj);
+            _context.SaveChanges();
+        }
     
 
     }
