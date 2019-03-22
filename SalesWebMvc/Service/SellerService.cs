16f96415e95a2b292c98d56bb8cd9bd52dc6cfcd
Lucas M da Silva <lucasmancini1997@gmail.com>
2019-03-22 11:00:09 -0300

    Integridade Referencial

--- SalesWebMvc/Service/SellerService.cs
+++ SalesWebMvc/Service/SellerService.cs
@@ -37,9 +37,16 @@ namespace SalesWebMvc.Service
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Sellers.FindAsync(id);
-            _context.Sellers.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Sellers.FindAsync(id);
+                _context.Sellers.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdateAsync(Sellers obj)
